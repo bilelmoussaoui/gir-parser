bafd04ae596b1c146bc3d4e69ae1ef38ec7e1886
Bilal Elmoussaoui <belmouss@redhat.com>
2024-06-14 18:22:47 +0200

    Be smarter when parsing versions

--- src/version.rs
+++ src/version.rs
@@ -4,22 +4,22 @@ use serde::Deserialize;
 use xmlserde::XmlValue;
 
 #[derive(Debug, Default, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Version(u16, u16, u16);
+pub struct Version(u16, Option<u16>, Option<u16>);
 
 impl Version {
     pub fn new(major: u16, minor: u16, patch: u16) -> Self {
-        Self(major, minor, patch)
+        Self(major, Some(minor), Some(patch))
     }
 
     pub fn major(self) -> u16 {
         self.0
     }
 
-    pub fn minor(self) -> u16 {
+    pub fn minor(self) -> Option<u16> {
         self.1
     }
 
-    pub fn patch(self) -> u16 {
+    pub fn patch(self) -> Option<u16> {
         self.2
     }
 }
@@ -46,14 +46,10 @@ impl FromStr for Version {
                 .map(str::parse)
                 .take_while(Result::is_ok)
                 .map(Result::unwrap);
-            Ok(Self(
-                parts.next().unwrap_or(0),
-                parts.next().unwrap_or(0),
-                parts.next().unwrap_or(0),
-            ))
+            Ok(Self(parts.next().unwrap_or(0), parts.next(), parts.next()))
         } else {
             let val = s.parse::<u16>();
-            Ok(Self(val.unwrap_or(0), 0, 0))
+            Ok(Self(val.unwrap_or(0), None, None))
         }
     }
 }
@@ -71,8 +67,10 @@ impl XmlValue for Version {
 impl std::fmt::Display for Version {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         match *self {
-            Self(major, minor, 0) => write!(f, "{major}.{minor}"),
-            Self(major, minor, patch) => write!(f, "{major}.{minor}.{patch}"),
+            Self(major, None, None) => write!(f, "{major}"),
+            Self(major, Some(minor), None) => write!(f, "{major}.{minor}"),
+            Self(major, None, Some(patch)) => write!(f, "{major}.0.{patch}"),
+            Self(major, Some(minor), Some(patch)) => write!(f, "{major}.{minor}.{patch}"),
         }
     }
 }
